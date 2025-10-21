c612ebe9aa7f12b0681c92ffcd64068356f7d0f0
Jhon Bueno <jhonbuenoa@gmail.com>
2025-10-21 15:03:48 -0500

    Agrega el ejercicio #1 completo junto con el script en index.html

---
+++ ejercicio1.js
@@ -0,0 +1,9 @@
+console.log("Números del 1 al 10 y si son pares:");
+
+// Usamos for para el ciclo y le aplicamos una condición if para verificar si el número es par:
+
+for (let i=1; i <=10; i++) {
+  if (i % 2 === 0) {
+    console.log(`El número ${i} es par`);
+  }
+} 
